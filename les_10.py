86ed0caee63cbf234f4950023615cfee09aa4d78
Yuriy Kovalev <yuriykovalev1984@gmail.com>
2024-02-06 19:23:11 +0300

    решение

--- les_10.py
+++ les_10.py
@@ -1,6 +1,13 @@
+import pandas as pd 
+import numpy as np 
 import random
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-data = pd.DataFrame({'whoAmI':lst})
-data.head()
+data = pd.DataFrame({'whoAmI': lst})
+data['tmp'] = 1
+data.set_index([data.index+1, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value = 0).astype(int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+print(data)
